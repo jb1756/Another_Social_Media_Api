a481993e26f2b75b344d7f43781b4fc693b02677
jb1756 <juliusbags04@yahoo.com>
2023-07-24 19:42:02 -0400

    models schemas

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,1 +1,31 @@
 const mongoose = require ('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+     thoughtText:
+    {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+    
+    ],
+  });
+  
+  // Create a virtual called reactionCount 
+  thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+  
+const Thought = mongoose.model('Thought', thoughtSchema);
+  
+module.exports = Thought;
--- models/Users.js
+++ models/Users.js
@@ -1,1 +1,38 @@
 const mongoose = require ('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, //use regex
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ]
+  });
+
+//Virtual friendcount
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+  
+  
+const User = mongoose.model('User', userSchema);
+  
+module.exports = User;
